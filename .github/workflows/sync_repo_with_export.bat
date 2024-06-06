5aea055916add2ed4abf26e7dffa42d85bb343ba
stepgo <167748477+stepg0@users.noreply.github.com>
2024-06-06 13:10:29 +0200

    Add files via upload

---
+++ .github/workflows/sync_repo_with_export.bat
@@ -0,0 +1,70 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+set "fileMask=*"
+
+REM FolderWithChanges is the exported power apps
+set "FolderWithChanges=C:\Users\stepgo\Documents\Capgemini\Training\PowerApps\2024\experiments\copy batches\powerskillbooster"
+REM FolderStartingPoint is the local repo
+set "FolderStartingPoint=C:\Users\stepgo\Documents\Capgemini\Training\PowerApps\2024\powerskillbooster\powerskillbooster"
+
+
+REM fc https://learn.microsoft.com/de-de/windows-server/administration/windows-commands/fc result of fc -> ERRORLEVEL
+REM errorlevel: 0  / No differences encountered -> do not overwrite
+REM errorlevel: 1  / Different file -> take file from FolderWithChanges
+REM errorlevel: 2  / Cannot find file ->  -> take file from FolderWithChanges
+REM errorlevel: -1 / Invalid syntax
+
+REM ----------- START DELETE from REPO --------------
+REM list all folders in FolderStartingPoint recursively
+for /f "delims=" %%D in ('echo "."^&forfiles /s /p "%FolderStartingPoint%" /m "%fileMask%" /c "cmd /c if @isdir==TRUE echo @relpath"') do (
+	REM debug echo directory "%%D"
+	
+	for /f "delims=" %%F in ('dir /b "%FolderStartingPoint%\%%D"') do (
+		REM ignore folders
+		if NOT exist "%FolderStartingPoint%\%%D\%%F\*" (
+			REM Cannot find file -> remove from folder
+			if NOT exist "%FolderWithChanges%\%%D\%%F" (
+				del "%FolderStartingPoint%\%%D\%%F"
+				echo Remove from FolderStartingPoint "%%F"
+			)
+			REM debug echo file "%%F"
+		)
+	)
+)
+
+REM remove empty folders
+for /f "delims=" %%d in ('dir "%FolderStartingPoint%" /s /b /ad ^| sort /r') do rd "%%d" > NUL 2>NUL
+
+REM ----------- END DELETE from REPO --------------
+
+
+
+REM ----------- START COPY from EXPORT --------------
+REM list all folders in FolderWithChanges recursively
+for /f "delims=" %%D in ('echo "."^&forfiles /s /p "%FolderWithChanges%" /m "%fileMask%" /c "cmd /c if @isdir==TRUE echo @relpath"') do (
+	REM debug echo directory "%%D"
+	
+	for /f "delims=" %%F in ('dir /b "%FolderWithChanges%\%%D"') do (
+		REM ignore folders
+		if NOT exist "%FolderWithChanges%\%%D\%%F\*" (
+			if exist "%FolderStartingPoint%\%%D\%%F" (
+				REM compare files in FolderStartingPoint, remove "> NUL 2>NUL" parameter to check compare result, replace with  >> out.txt to write echo to file
+				fc /l /t "%FolderWithChanges%\%%D\%%F" "%FolderStartingPoint%\%%D\%%F" > NUL 2>NUL
+				REM debug echo file comparison result "%%F" -- "!errorlevel!" >> out.txt
+				REM Different file -> take file from FolderWithChanges
+				if "!errorlevel!" EQU "1" (
+					copy "%FolderWithChanges%\%%D\%%F" "%FolderStartingPoint%\%%D\%%F" 
+					echo Copied from FolderWithChanges "%%F"
+				)
+			) else (
+				REM Cannot find file -> take file from FolderWithChanges
+				xcopy /S /Q /Y /F "%FolderWithChanges%\%%D\%%F" "%FolderStartingPoint%\%%D\%%F\" REM create folder structure first
+				copy "%FolderWithChanges%\%%D\%%F" "%FolderStartingPoint%\%%D\%%F" REM copy file
+				echo Copied from FolderWithChanges "%%F"
+			)
+			REM debug echo file "%%F"
+		)
+	)
+)
+REM ----------- END COPY from EXPORT --------------
